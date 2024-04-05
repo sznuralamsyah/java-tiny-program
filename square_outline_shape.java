9da889ee4fb0001ba1bead5f2376e7fe369d99d7
Andika Nuralamsyah <118785064+sznuralamsyah@users.noreply.github.com>
2024-04-06 03:49:17 +0700

    draw square outline shape with * in java

---
+++ square_outline_shape.java
@@ -0,0 +1,15 @@
+public class square_outline_shape {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == 0 || j == 0 || i == 10 - 1 || j == 10 - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
