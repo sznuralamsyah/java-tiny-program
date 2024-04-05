e402c0b5bb89d66836eab15e088359ab154adb28
Andika Nuralamsyah <118785064+sznuralamsyah@users.noreply.github.com>
2024-04-06 03:11:28 +0700

    draw rectangle with * in java

---
+++ rectangleshape.java
@@ -0,0 +1,20 @@
+public class rectangleshape {
+    public static void main(String[] args) {
+
+        System.out.println("Rectangle Kebawah");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("Rectangle Kesamping");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 25; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
